50a3e266d064edcc97e3e9696575670727045aac
rileyjones-stack <65572687+rileyjones-stack@users.noreply.github.com>
2020-09-21 09:33:04 +1200

    Add files via upload

---
+++ consumption.py
@@ -0,0 +1,7 @@
+form_of_consumption = input("Delivery or pickup? ")
+if form_of_consumption == 'delivery':
+    print("Delivery it is")
+elif form_of_consumption == 'pickup':
+    print("Pickup it is")
+else: 
+    print("That is not a valid form of consumption.")
